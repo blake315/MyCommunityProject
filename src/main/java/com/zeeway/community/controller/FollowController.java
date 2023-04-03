4217387cb154ed89eb15f84f62bbbfcea034845d
thales <1455975369@qq.com>
2023-04-03 20:06:31 +0800

    展现关注用户列表和用户的粉丝列表

--- src/main/java/com/zeeway/community/controller/FollowController.java
+++ src/main/java/com/zeeway/community/controller/FollowController.java
@@ -1,19 +1,31 @@
 package com.zeeway.community.controller;
 
+import com.zeeway.community.entity.Page;
 import com.zeeway.community.entity.User;
 import com.zeeway.community.service.FollowService;
+import com.zeeway.community.service.UserService;
+import com.zeeway.community.util.CommunityConstant;
 import com.zeeway.community.util.CommunityUtil;
 import com.zeeway.community.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+import java.util.Map;
+
 @Controller
-public class FollowController {
+public class FollowController implements CommunityConstant {
 
     @Autowired
     private FollowService followService;
+
+    @Autowired
+    private UserService userService;
 
     @Autowired
     private HostHolder hostHolder;
@@ -34,5 +46,66 @@ public class FollowController {
         followService.unfollow(user.getId(), entityType, entityId);
 
         return CommunityUtil.getJSONString(0,"unfollowed!");
+    }
+
+    @GetMapping(path = "/followees/{userId}")
+    public String getFollowee(@PathVariable("userId") int userId, Page page, Model model){
+        final User user = userService.findUserById(userId);
+        if (user == null){
+            throw new RuntimeException("user is not exit");
+        }
+        model.addAttribute("user", user);
+
+        page.setLimit(5);
+        page.setPath("/followees/" + userId);
+        page.setRows((int) followService.findFolloweeCount(userId,CommunityConstant.ENTITY_TYPE_USER));
+
+        List<Map<String, Object>> userList = followService.findFollowee(userId, page.getOffset(), page.getLimit());
+        if (userList != null){
+            for (Map<String, Object> map: userList){
+                User u = (User) map.get("user");
+                map.put("hasFollowed", hasFollowed(u.getId()));
+            }
+        }
+
+        model.addAttribute("users", userList);
+
+        return "/site/followee";
+
+    }
+
+
+    @GetMapping(path = "/followers/{userId}")
+    public String getFollower(@PathVariable("userId") int userId, Page page, Model model){
+        final User user = userService.findUserById(userId);
+        if (user == null){
+            throw new RuntimeException("user is not exit");
+        }
+        model.addAttribute("user", user);
+
+        page.setLimit(5);
+        page.setPath("/followers/" + userId);
+        page.setRows((int) followService.findFollowerCount(CommunityConstant.ENTITY_TYPE_USER, userId));
+
+        List<Map<String, Object>> userList = followService.findFollower(userId, page.getOffset(), page.getLimit());
+        if (userList != null){
+            for (Map<String, Object> map: userList){
+                User u = (User) map.get("user");
+                map.put("hasFollowed", hasFollowed(u.getId()));
+            }
+        }
+
+        model.addAttribute("users", userList);
+
+        return "/site/follower";
+
+    }
+
+
+    private boolean hasFollowed(int userId){
+        if (hostHolder.getUser() == null){
+            return false;
+        }
+        return followService.hasFollowed(hostHolder.getUser().getId(), CommunityConstant.ENTITY_TYPE_USER, userId);
     }
 }
--- src/main/java/com/zeeway/community/service/FollowService.java
+++ src/main/java/com/zeeway/community/service/FollowService.java
@@ -1,5 +1,7 @@
 package com.zeeway.community.service;
 
+import com.zeeway.community.entity.User;
+import com.zeeway.community.util.CommunityConstant;
 import com.zeeway.community.util.RedisKeyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
@@ -8,11 +10,16 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.SessionCallback;
 import org.springframework.stereotype.Service;
 
+import java.util.*;
+
 @Service
-public class FollowService {
+public class FollowService implements CommunityConstant {
 
     @Autowired
     private RedisTemplate redisTemplate;
+
+    @Autowired
+    private UserService userService;
 
     public void follow(int userId, int entityType, int entityId){
         redisTemplate.execute(new SessionCallback() {
@@ -62,5 +69,48 @@ public class FollowService {
     public boolean hasFollowed(int userId, int entityType, int entityId){
         String followeeKey = RedisKeyUtil.getFolloweeKey(userId, entityType);
         return redisTemplate.opsForZSet().score(followeeKey, entityId) != null;
+    }
+
+    //查询关注列表
+    public List<Map<String, Object>> findFollowee(int userId, int offset, int limit){
+        String followeeKey = RedisKeyUtil.getFolloweeKey(userId, ENTITY_TYPE_USER);
+        Set<Integer> targetIds =  redisTemplate.opsForZSet().reverseRange(followeeKey,offset, offset + limit - 1);
+        if (targetIds == null){
+            return null;
+        }
+
+        List<Map<String, Object>> list = new ArrayList<>();
+        for (Integer targetId : targetIds){
+            Map<String, Object> map = new HashMap<>();
+            User user = userService.findUserById(targetId);
+            map.put("user", user);
+            Double score = redisTemplate.opsForZSet().score(followeeKey, targetId);
+            map.put("followTime", new Date(score.longValue()));
+            list.add(map);
+        }
+
+        return list;
+    }
+
+    //查询粉丝列表
+    public List<Map<String, Object>> findFollower (int userId, int offset, int limit){
+        String followerKey = RedisKeyUtil.getFollowerKey(ENTITY_TYPE_USER, userId);
+        final Set<Integer> targetIds = redisTemplate.opsForZSet().reverseRange(followerKey, offset, offset + limit - 1);
+
+        if (targetIds == null){
+            return null;
+        }
+
+        List<Map<String, Object>> list = new ArrayList<>();
+        for (Integer targetId : targetIds){
+            Map<String, Object> map = new HashMap<>();
+            User user = userService.findUserById(targetId);
+            map.put("user", user);
+            Double score = redisTemplate.opsForZSet().score(followerKey, targetId);
+            map.put("followTime", new Date(score.longValue()));
+            list.add(map);
+        }
+
+        return list;
     }
 }
